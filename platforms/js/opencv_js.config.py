440eb849373c28d76efdb79b5e6f4fd518f58abb
Bogdan Sbiera <Bogdan.Sbiera@cloudflight.io>
2020-05-11 18:32:51 +0300

    strip out some more core methods

--- platforms/js/opencv_js.config.py
+++ platforms/js/opencv_js.config.py
@@ -1,10 +1,5 @@
 # Classes and methods whitelist
-core = {'': ['absdiff', 'add', 'addWeighted', 'bitwise_and', 'bitwise_not', 'bitwise_or', 'bitwise_xor', 'cartToPolar',\
-             'compare', 'convertScaleAbs', 'countNonZero', 'determinant', 'dft', 'divide', 'eigen', \
-             'exp', 'flip', 'gemm', 'hconcat', 'inRange', 'invert', 'kmeans', 'log', 'magnitude', \
-             'max', 'mean', 'meanStdDev', 'merge', 'min', 'minMaxLoc', 'mixChannels', 'multiply', 'norm', 'normalize', \
-             'perspectiveTransform', 'polarToCart', 'pow', 'randn', 'randu', 'reduce', 'repeat', 'setIdentity', 'setRNGSeed', \
-             'solve', 'solvePoly', 'split', 'sqrt', 'subtract', 'trace', 'transform', 'transpose', 'vconcat'],
+core = {'': [],
         'Algorithm': []}
 
 imgproc = {'': [ 'resize'],}
